b3e093ac68ad090479ba484421b5e9770dbbb730
team5official <67334232+team5official@users.noreply.github.com>
2020-07-30 02:23:10 +0530

    Add files via upload

---
+++ lib/components/mhSP.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SP {
+  List<Text> _SP = [
+    Text(
+      '1)	using mood-altering substances, including alcohol and nicotine, more freq',
+    ),
+    Text(
+      '2)	sleeping too much or too little',
+    ),
+    Text(
+      '3) withdrawing from friends, family, and colleagues',
+    ),
+  ];
+}
